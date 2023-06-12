fd41997dd5465100647a27eaa0ca3a9222ab903d
nyk <nykcasseano13@gmail.com>
2023-06-12 11:56:14 -0300

    último commit

--- Solutiuon_hint4&5.py
+++ Solutiuon_hint4&5.py
@@ -6,12 +6,28 @@
 #Difficulty Expert 🤯: Only use Hint 1 to complete the project.
 
 ############### Our Blackjack House Rules #####################
-
+import random
 ## The deck is unlimited in size. 
 ## There are no jokers. 
 ## The Jack/Queen/King all count as 10.
 ## The the Ace can count as 11 or 1.
 ## Use the following list as the deck of cards:
+def deal_card():
+    
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    cards = random.choice(cards)
+    return cards
+#Hint 5: Deal the user and computer 2 cards each using deal_cards()
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+
+
+
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 ## The cards in the list have equal probability of being drawn.
 ## Cards are not removed from the deck as they are drawn.
